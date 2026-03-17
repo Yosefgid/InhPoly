7e05d3d2e8a94526485f39845395a3aecd01a482
Yosefgid <jossykasshun@yahoo.com>
2026-03-17 11:36:39 +0000

    Add project files.

---
+++ Vehicles/Vehicle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles
+{
+    public abstract class Vehicle
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Speed { get; set; }
+
+        //Create Constructor
+        public Vehicle(string MAKE, string model, int speed)
+        {
+            Make = make;
+            Model = model;
+            Speed = speed;
+        }
+    }
+}
