c642aca34bb597e28c3e6f9e01ddf93b58766014
Yosefgid <jossykasshun@yahoo.com>
2026-03-17 12:39:45 +0000

    Add an abstract class Engine

---
+++ Vehicles/Engine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles
+{
+    public abstract class Engine
+    {
+       public bool Running { get; set }
+        public int HorsePower { get; set }
+        public FuelType FuelType { get; set; }
+
+        public Engine(int horsePower, FuelType fuelType)
+        {
+            HorsePower = horsePower;
+            FuelType = fuelType;
+            Running = false;
+        }
+    }
+}
